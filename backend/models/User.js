baf038753eaff56558547541d8c508da8a75fbfd
lilsea7 <summersea.ae@gmail.com>
2025-10-27 09:04:47 +0700

    update user

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,22 +3,25 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Name is required'],
     trim: true,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Email is required'],
     unique: true,
     trim: true,
+    lowercase: true,
+    match: [/.+@.+\..+/, 'Invalid email format'],
   },
   age: {
     type: Number,
     required: true,
-    min: 0,
+    min: [0, 'Age must be >= 0'],
   },
 }, {
   timestamps: true,
 });
+
 
 module.exports = mongoose.model('User', userSchema);
